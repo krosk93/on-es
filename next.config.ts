0e3c68eed094dd8add8bb43abde14b3584f2c8b6
Rovira-Lopez Oscar <--get>
2024-12-05 16:22:45 +0100

    Static build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/on-es",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
